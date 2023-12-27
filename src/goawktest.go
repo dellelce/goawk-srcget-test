10eb356a4789fb00928ad56ff93ef26c569831f9
Antonio Dell'Elce <antouk@dellelce.com>
2023-12-27 22:52:37 +0000

    add initial code

--- src/goawktest.go
+++ src/goawktest.go
@@ -1,15 +1,67 @@
 package main
 
-import _ "embed"
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"github.com/benhoyt/goawk/interp"
+	"github.com/benhoyt/goawk/parser"
+	"io"
+	"net/http"
+)
 
-//go:embed awk/firstaftermatchword-trim.latest.awk
+// later: awk/firstaftermatchword-trim.latest.awk
+// go:embed awk/github.latest.awk
 var f1 []byte
 
-func embedtest() {
-	print(string(f1))
-	// awk/github.latest.awk
+func download(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		return bodyBytes, nil
+	}
+
+	return nil, err
 }
 
 func main() {
-	embedtest()
+	body, err := download("https://github.com/landley/toybox/tags")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("debug: github content:%s\n", body[:100])
+
+	source := f1
+	if len(source) == 0 {
+		fmt.Println("did not read awk source correctly")
+		return
+	}
+
+	fmt.Println("debug: awk file:%s", source[:100])
+
+	prog, err := parser.ParseProgram([]byte(source), nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	config := &interp.Config{
+		Stdin: bytes.NewBufferString(string(body)),
+	}
+
+	_, err = interp.ExecProgram(prog, config)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 }
